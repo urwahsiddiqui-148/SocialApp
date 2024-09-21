30a90c4391533e24161b52ad1b05b118109bb677
urwahsiddiqui-148 <urwahshafiq23@gmail.com>
2024-09-21 14:08:36 +0500

    Added the final project

---
+++ lib/app/configs/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:social_media_app/app/configs/colors.dart';
+
+class AppTheme {
+  static final TextStyle lightText = GoogleFonts.poppins(
+    color: AppColors.blackTextColor,
+  );
+
+  static TextStyle blackTextStyle = GoogleFonts.poppins(
+    color: AppColors.blackTextColor,
+  );
+  static TextStyle whiteTextStyle = GoogleFonts.poppins(
+    color: AppColors.whiteColor,
+  );
+  static TextStyle greyTextStyle = GoogleFonts.poppins(
+    color: AppColors.greyTextColor,
+  );
+
+  static FontWeight light = FontWeight.w300;
+  static FontWeight regular = FontWeight.w400;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight extraBold = FontWeight.w800;
+  static FontWeight black = FontWeight.w900;
+
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.whiteColor,
+    navigationBarTheme: const NavigationBarThemeData(
+      // indicatorColor: AppColors.backgroundColor,
+      indicatorColor: AppColors.whiteColor,
+    ),
+  );
+}
